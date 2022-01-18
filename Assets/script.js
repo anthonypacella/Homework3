cea1a23c42a8eb80cfa7efc6c1cc6593339c0d3d
anthonypacella <anthonyapacella@gmail.com>
2022-01-18 16:37:06 -0500

    Updating javascript and README

--- Assets/script.js
+++ Assets/script.js
@@ -14,6 +14,7 @@ var random;
 var passwordArray = [];
 var passwordString;
 var password;
+var arrayLength;
 
 function requestCharacterLength () {
     characterLength = prompt("How many characters would you like your password to be?");
@@ -31,9 +32,6 @@ function requestCharacterLength () {
       }
 }
 
-requestCharacterLength();
-checkIncludedCharacters();
-
 function checkIncludedCharacters() {
     lowercaseIncluded = confirm("Click OK if you want lowercase letters included in your password.");
     uppercaseIncluded = confirm("Click OK if you want uppercase letters included in your password.");
@@ -48,83 +46,85 @@ function checkIncludedCharacters() {
 
 }
 
+function combineArrays () {
 
-if (lowercaseIncluded) {
+    if (lowercaseIncluded) {
 
-  lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+      lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    
+    }
+    
+    else if (!lowercaseIncluded) {
+    
+      lowercaseArray = [];
+    
+    }
+    
+    if (uppercaseIncluded) {
+    
+      uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    
+    }
+    
+    else if (!uppercaseIncluded) {
+    
+      uppercaseArray = [];
+    
+    }
+    
+    if (specialIncluded) {
+    
+      specialArray = ["!", "#", "$", "%", "&" ,"'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";" , "<" , "=" , ">" , "?", "@" , "[", "^", "_" , "`", "{", "|", "}", "~"];
+    
+    }
+    
+    else if (!specialIncluded) {
+    
+      specialArray = [];
+    
+    }
+    
+    if (numberIncluded) {
+    
+      numberArray = [0,1,2,3,4,5,6,7,8,9];
+    
+    }
+    
+    else if (!numberIncluded) {
+    
+      numberArray = [];
+    
+    }
+    
+    if (lowercaseIncluded) {
+    
+      fullCharacterArray = lowercaseArray.concat(uppercaseArray, specialArray, numberArray);
+    }
+    
+    else if (uppercaseIncluded) {
+    
+      fullCharacterArray = uppercaseArray.concat(lowercaseArray, specialArray, numberArray);
+    
+    }
+    
+    else if (specialIncluded) {
+    
+      fullCharacterArray = specialArray.concat(lowercaseArray, uppercaseArray, numberArray);
+    
+    }
+    
+    else if (numberIncluded) {
+    
+      fullCharacterArray = numberArray.concat(lowercaseArray, uppercaseArray, specialArray);
+    
+    }
 }
-
-else if (!lowercaseIncluded) {
-
-  lowercaseArray = [];
-
-}
-
-if (uppercaseIncluded) {
-
-  uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-}
-
-else if (!uppercaseIncluded) {
-
-  uppercaseArray = [];
-
-}
-
-if (specialIncluded) {
-
-  specialArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}",":",";","<",",",">",".","?","/"];
-
-}
-
-else if (!specialIncluded) {
-
-  specialArray = [];
-
-}
-
-if (numberIncluded) {
-
-  numberArray = [0,1,2,3,4,5,6,7,8,9];
-
-}
-
-else if (!numberIncluded) {
-
-  numberArray = [];
-
-}
-
-if (lowercaseIncluded) {
-
-  fullCharacterArray = lowercaseArray.concat(uppercaseArray, specialArray, numberArray);
-}
-
-else if (uppercaseIncluded) {
-
-  fullCharacterArray = uppercaseArray.concat(lowercaseArray, specialArray, numberArray);
-
-}
-
-else if (specialIncluded) {
-
-  fullCharacterArray = specialArray.concat(lowercaseArray, uppercaseArray, numberArray);
-
-}
-
-else if (numberIncluded) {
-
-  fullCharacterArray = numberArray.concat(lowercaseArray, uppercaseArray, specialArray);
-
-}
-
-var arrayLength = fullCharacterArray.length;
-
 
 // Write password to the #password input
 function writePassword() {
+  
+  requestCharacterLength();
+  checkIncludedCharacters();
 
   password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -133,9 +133,11 @@ function writePassword() {
 }
 
 function generatePassword() {
-
   passwordArray = [];
+  combineArrays();
 
+  arrayLength = fullCharacterArray.length;
+  
   for (var i = 0; i < characterLength; i++) {
 
       random = Math.floor(Math.random() * arrayLength);
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Homework3
+# 03 JavaScript: Password Generator
+
+## Overview:
+
+In this week’s homework, a password generator was developed to help employees generate random passwords based on criteria that they’ve selected. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript. 
+
+## Features
+
+```
+Upon viewing the page, the user is shown a page with the title "Password Generator," and a subtitle "Generate Password.:
+There is also a password box shown, with a generate password button.
+When the user wants to generate a password, they will hit the Generate Password button and be greeted with a series of prompts that will indicate the criteria for the generated password.
+
+The first prompt will indicate the password length (which needs to be between 8 and 128 characters and is validated by the code)
+The the user will be ask whether or not to include lowercase, uppercase, numeric, and/or special characters. The user can select "OK" or "cancel" to make their choice.
+At least one character type must be chosen.
+Once the criteria is selected, the password will be shown in the password box.
+
+The user can then click the generate passwork button again to generate a new password if they wish, and will be prompted again to choose their criteria.
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance:
+
+![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
